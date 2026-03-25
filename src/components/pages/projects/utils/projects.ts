70260352d67f7b420bb34dc174501f84df248b19
yousufrenexus-del <yousufrenexus@gmail.com>
2026-03-25 16:10:36 +0500

    Updated code No error

--- src/components/pages/projects/utils/projects.ts
+++ src/components/pages/projects/utils/projects.ts
@@ -2,13 +2,21 @@ import Project1 from '@/../public/assets/images/project1.jpg';
 import Project2 from '@/../public/assets/images/project2.jpg';
 import Project3 from '@/../public/assets/images/project3.png';
 import Project4 from '@/../public/assets/images/project4.jpg';
+import { StaticImageData } from 'next/image';
 
-export const PROJECTS_ITEMS = {
+type ProjectItem = {
+  image: StaticImageData;
+  href?: string;
+  githubHref?: string;
+  tags: string[];
+};
+
+export const PROJECTS_ITEMS: Record<string, ProjectItem> = {
   "Project_1": {
-    "image": Project1,
-    "href": "https://cfmoto.com.br/",
-    "githubHref": "",
-    "tags": [
+    image: Project1,
+    href: "https://cfmoto.com.br/",
+    githubHref: "",
+    tags: [
       "TypeScript",
       "Next.js",
       "React",
@@ -18,24 +26,10 @@ export const PROJECTS_ITEMS = {
       "Prisma",
     ],
   },
-  // "Project_2": {
-  //   "image": Project2,
-  //   "href": "https://catarinaaraujoadvocacia.adv.br/",
-  //   "githubHref": "",
-  //   "tags": [
-  //     "TypeScript",
-  //     "Next.js",
-  //     "React",
-  //     "Tailwind",
-  //     "Shadcn",
-  //     "Vercel",
-  //   ],
-  // },
   "Project_3": {
-    "image": Project3,
-    // "href": `${process.env.NEXT_PUBLIC_BASE_URL}`,
-    // "githubHref": `${process.env.NEXT_PUBLIC_PORTIFOLIO_URL}`,
-    "tags": [
+    image: Project3,
+    // href and githubHref are optional, so no error
+    tags: [
       "TypeScript",
       "Next.js",
       "React",
@@ -45,18 +39,4 @@ export const PROJECTS_ITEMS = {
       "Vercel",
     ],
   },
-  // "Project_4": {
-  //   "image": Project4,
-  //   "href": "",
-  //   "githubHref": "",
-  //   "tags": [
-  //     "TypeScript",
-  //     "Next.js",
-  //     "React",
-  //     "Tailwind",
-  //     "Shadcn",
-  //     "NestJS",
-  //     "Prisma",
-  //   ],
-  // }
-}
+};
