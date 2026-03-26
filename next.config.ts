c73fe4f251782e7888d861d0b5437c02a3dc33d5
SyedYousuf123 <syousufa68@gmail.com>
2026-03-26 11:20:21 +0500

    Wrong Redirection Resolved

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-import {NextConfig} from 'next';
+import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+
 const nextConfig: NextConfig = {
- 
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: false,
+      },
+    ];
+  },
 };
- 
+
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
